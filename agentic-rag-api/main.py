e57ec9a96718f0e5bb73a3de98259f96cf7e3808
Himanshu Raturi <raturihimanshu077@gmail.com>
2026-01-23 10:22:32 +0530

    Add lambda function zip and update requirements.txt for dependencies

--- agentic-rag-api/main.py
+++ agentic-rag-api/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from schema import QueryRequest , QueryResponse , AgentState
 from agentic_rag import ask_question
+from mangum import Mangum
 
 app = FastAPI(
     title="Agentic RAG API",
     version="1.0.0"
 )
-
+handler = Mangum(app)
 @app.post("/query",response_model=QueryResponse)
 def query_rag(request: QueryRequest):
     state = AgentState(question=request.question)
